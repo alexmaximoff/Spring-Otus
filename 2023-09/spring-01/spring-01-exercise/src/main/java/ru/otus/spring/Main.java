6a477e3a5bdbd989cc925726a2136b4dd972cfba
AlMaksimov <almaksimov@sportmaster.ru>
2023-09-29 10:03:52 +0300

    Выполнил практику к первому уровку

--- 2023-09/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2023-09/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -1,17 +1,22 @@
 package ru.otus.spring;
 
 //import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
 
 public class Main {
 
     public static void main(String[] args) {
         // TODO: создайте здесь класс контекста
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
 
         // TODO: Получите Person Service
+        PersonService personService = context.getBean("personService", PersonService.class);
 
         // Получите Person "Ivan"
-        Person ivan = null;
+        Person ivan = personService.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
 }
